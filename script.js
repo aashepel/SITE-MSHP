489801cf39bb517b85c8b3cbfa391cb9ec61239f
aashepel <aashepel2002@mail.ru>
2020-06-24 23:15:33 +0300

    modification mobile menu

--- script.js
+++ script.js
@@ -13,13 +13,26 @@ window.onload = function () {
     let element_active_link = document.getElementById('active');
     document.getElementsByTagName('h1')[0].textContent = element_active_link.textContent;
     document.getElementsByTagName("title")[0].textContent = element_active_link.textContent;
-    showSlides(1, document.getElementById('brief-information'));
-    showSlides(1, document.getElementById('popular-cars'));
-    showSlides(1, document.getElementById('last-cars'))
-    let images = document.getElementsByTagName('img');
-    for (let i = 0; i < images.length; i++){
-        images[i].alt = "Ошибка загрузки изображения";
+    if (document.getElementById('brief-information')) {
+        showSlides(1, document.getElementById('brief-information'));
+        showSlides(1, document.getElementById('popular-cars'));
+        showSlides(1, document.getElementById('last-cars'))
+        let images = document.getElementsByTagName('img');
+        for (let i = 0; i < images.length; i++) {
+            images[i].alt = "Ошибка загрузки изображения";
+        }
     }
+    // console.log('100');
+    let element_li = document.getElementsByTagName('li');
+    // console.log(element_li.length);
+    for (let i = 0; i < element_li.length; i++){
+        element_li[i].addEventListener("click", () => {
+           if (document.getElementById('mobile-menu').style.display == "inline-block"){
+               open_mobile_menu();
+           }
+        });
+    }
+
 }
 
 let slideIndex = 1;
@@ -59,4 +72,16 @@ function showGallery(element) {
         element.style.display = "none";
     }
 
+}
+
+function open_mobile_menu() {
+    if (document.getElementById('mobile-menu').style.display != 'inline-block') {
+        document.getElementById('mobile-menu').style.display = "inline-block";
+        document.getElementById('btn-open-menu-on-mobile-outside').style.display = "none";
+        document.body.style.overflow = "hidden";
+    } else {
+        document.getElementById('mobile-menu').style.display = "none";
+        document.getElementById('btn-open-menu-on-mobile-outside').style.display = "inline-block";
+        document.body.style.overflow = "visible";
+    }
 }
